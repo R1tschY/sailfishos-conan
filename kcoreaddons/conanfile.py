3d244c7588a9c7a93a2ef049dbfeeebe0e2d38c6
R1tschY <r1tschy@yahoo.de>
2019-03-17 21:12:15 +0100

    Added KCoreAddons recipe

---
+++ kcoreaddons/conanfile.py
@@ -0,0 +1,60 @@
+from conans import ConanFile, tools, CMake
+import os
+import textwrap
+
+
+class KCoreAddonsConanFile(ConanFile):
+    name = "KCoreAddons"
+    version = "5.36.0"
+    description = "Addons to QtCore"
+    license = "LGPLv2"
+    url = "https://github.com/r1tschy/conan-kf5"
+
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake"
+
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+
+    download_folder = "%s-%s" % (name.lower(), version)
+    short_version = ".".join(version.split(".")[:2])
+
+    def source(self):
+        zip_name = "%s.zip" % self.download_folder
+        url = "http://download.kde.org/stable/frameworks/%s/%s" % (
+            self.short_version,
+            zip_name,
+        )
+        self.output.info("Downloading %s ..." % url)
+        tools.download(url, zip_name)
+        tools.unzip(zip_name)
+        os.unlink(zip_name)
+
+        tools.replace_in_file(
+            "%s/CMakeLists.txt" % self.download_folder,
+            "include(FeatureSummary)",
+            textwrap.dedent(
+                """
+                include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+                conan_basic_setup()
+                include(FeatureSummary)"""
+            ),
+        )
+
+    def build(self):
+        cmake = CMake(self, generator="Ninja")
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = os.path.join(
+            self.build_folder, "install"
+        )
+        cmake.configure(source_folder=self.download_folder)
+        cmake.build()
+        self.run("cmake --build . --target install")
+
+    def package(self):
+        self.copy("*", dst="include", src="install/include/KF5/%s" % self.name)
+        self.copy("*.a", dst="lib", src="install/lib", keep_path=False)
+        self.copy("*.so", dst="lib", src="install/lib", keep_path=False)
+        self.copy("*.qm", dst="share/locale", src="install/share", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = [self.name.replace("K", "KF5", 1)]
---
+++ kcoreaddons/test_package/example.cpp
@@ -0,0 +1,8 @@
+#include <KAboutData>
+#include <QDebug>
+
+int main()
+{
+  qDebug() << KAboutData::aboutTranslationTeam();
+  return 0;
+}
