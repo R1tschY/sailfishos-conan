a3512d26e7f47dfdc8f4ba29cf370e542421bd10
R1tschY <r1tschy@yahoo.de>
2019-05-08 16:39:07 +0200

    Added KCoreAddons to CI

--- ci_build.py
+++ ci_build.py
@@ -1,17 +1,22 @@
 from cpt.packager import ConanMultiPackager
 import subprocess
+import argparse
 
 
 CONAN_ARCH = {"i486": "x86", "armv7l": "armv7", "armv7hl": "armv7"}
 
 
 if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("path", default=None)
+    args = argparser.parse_args()
+
     arch = subprocess.check_output("uname -m", shell=True, encoding="utf-8").strip()
     if not arch:
         raise LookupError("Failed to get gcc compile arch")
     conan_arch = CONAN_ARCH.get(arch, arch)
 
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(cwd=args.path)
     builder.add(
         settings={
             "arch": conan_arch,
