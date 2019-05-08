ceb7f723449c35f187afd2906784cf6c90d2227d
R1tschY <r1tschy@yahoo.de>
2019-05-08 15:06:57 +0200

    Map to conan arch

--- libssh/ci_build.py
+++ libssh/ci_build.py
@@ -2,23 +2,19 @@ from cpt.packager import ConanMultiPackager
 import subprocess
 
 
+CONAN_ARCH = {"i486": "x86", "armv7h": "armv7", "armv7hl": "armv7"}
+
+
 if __name__ == "__main__":
     arch = subprocess.check_output("uname -m", shell=True)
     if not arch:
         raise LookupError("Failed to get gcc compile arch")
     arch = arch.decode("utf-8")
+    conan_arch = CONAN_ARCH.get(arch, arch)
 
     builder = ConanMultiPackager()
     builder.add(
-        settings={
-            "os": "Linux",
-            "os_build": "Linux",
-            "compiler": "gcc",
-            "compiler.libcxx": "libstdc++",
-            "arch": arch,
-            "arch_build": arch,
-            "build_type": "Release",
-        },
+        settings={"arch": conan_arch, "build_type": "Release"},
         options={},
         env_vars={},
         build_requires={},
