97be2beecebd6e342e9c3551dda8fda8ddc44708
R1tschY <r1tschy@yahoo.de>
2019-05-18 01:14:17 +0200

    CI: Fix KCoreAddon build with cpt
    Get package name and version on our own to work-a-round early module
    import in cpt. This early module import is a problem for python_requires
    usages.

--- ci_build.py
+++ ci_build.py
@@ -2,11 +2,37 @@ from cpt.packager import ConanMultiPackager
 import subprocess
 import argparse
 import os
+import ast
 
 CONAN_ARCH = {"i486": "x86", "armv7l": "armv7", "armv7hl": "armv7"}
 
 
-if __name__ == "__main__":
+def get_reference(file_path: str):
+    with open(file_path, "r", encoding="utf-8") as fp:
+        content = fp.read()
+
+    module = ast.parse(content, file_path)
+    conan_file = next(
+        (stmt for stmt in module.body if isinstance(stmt, ast.ClassDef)), None
+    )
+    if conan_file is None:
+        raise RuntimeError(f"missing conan class in {file_path}")
+
+    name = version = None
+    for stmt in conan_file.body:
+        if isinstance(stmt, ast.Assign):
+            if any(e.id == "name" for e in stmt.targets):
+                name = ast.literal_eval(stmt.value)
+            if any(e.id == "version" for e in stmt.targets):
+                version = ast.literal_eval(stmt.value)
+
+    if name is None or version is None:
+        raise RuntimeError(f"name or version missing in conan file {file_path}")
+
+    return f"{name}/{version}"
+
+
+def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument("path", default=None)
     args = argparser.parse_args()
@@ -21,9 +47,14 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(
         remotes=[
             ("https://api.bintray.com/conan/r1tschy/sailfishos", True, "sailfishos")
-        ]
+        ],
+        reference=get_reference("conanfile.py"),
     )
     builder.add(
         settings={"arch": conan_arch, "arch_build": conan_arch, "build_type": "Release"}
     )
     builder.run()
+
+
+if __name__ == "__main__":
+    main()
